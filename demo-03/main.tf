0330b6dee77f44264f93246820100f413adbc639
Raghuveer814 <raghuveer814@gmail.com>
2023-04-07 10:58:51 +0530

    third commit

--- demo-03/main.tf
+++ demo-03/main.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "C"
       labels = {
         my_label = "value"
       }
